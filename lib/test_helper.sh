bd341efea8054d8699d10e81c2f50f74bae9a474
Tim Uruski <github@login.timuruski.name>
2014-11-22 15:26:34 -0700

    Add optional description to test_helper

--- lib/test_helper.sh
+++ lib/test_helper.sh
@@ -5,14 +5,15 @@ solve() {
 }
 
 fail() {
- echo >&2 "Fail: ${1}"
+ echo >&2 "Fail: $1"
  exit 1
 }
 
 assert_eql() {
-  expected="${1}"
-  actual="${2}"
-  message="Expected \"${expected}\" to equal \"${actual}\""
+  expected="$1"
+  actual="$2"
+  desc="$3"
+  message="Expected \"$expected\" to equal \"$actual\""
 
-  [[ $actual == $expected ]] || fail "${message}"
+  [[ $actual == $expected ]] || fail "$message $desc"
 }
